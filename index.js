c9a53766a804201c7c4bae9bda355abbbb239197
Seth Bennett <toerag9331@gmail.com>
2022-12-05 15:04:59 +0000

    Fixed rotating head and added meters

--- index.js
+++ index.js
@@ -1,33 +1,48 @@
-    function getName()
-    {
-        const nameInput = document.querySelector('.name');
-        const petName = nameInput.value;
-        const petConfirm = document.querySelector('#petConfirm');
-        petConfirm.textContent = `Your pet is called ${petName}`; //make it so you can only select you name once
-        console.log(petName);
-    }
-    function getType(e)
-    {
-        const petType = e.target.value;
-        console.log(petType);
-        if(petType.value == 'cat')
-        {
+const pet={
+    petName: '',
+    petType: '',
+    hunger: 33,
+    dirtiness: 33,
+    sleep: 33,
+    happiness: 50
+}
+
+function getName(){
+    const nameInput = document.querySelector('.name');
+    pet.petName = nameInput.value;
+    const petConfirm = document.querySelector('#petConfirm');
+    petConfirm.textContent = `Your pet is called ${pet.petName}`; //make it so you can only select you name once
+}
+
+function getType(e){
+    pet.petType = e.target.value;
+    console.log(pet.petType);
+    if(pet.petType.value == 'cat'){
             const petImage = document.createElement('img')
             petImage.src = 'catIMG.jpg' 
-        }
-        else if(petType.value == 'dog')
-        {
+    }
+    else if(pet.petType.value == 'dog'){
             const petImage = document.createElement('img')
-            petImage.src = '.dogIMG.jpg'
-        }
-        else if(petType.value == 'rabbit')
-        {
+            petImage.src = '.dogIMG.jpg'}
+    else if(pet.petType.value == 'rabbit'){
             const petImage = document.createElement('img')
             petImage.src = 'rabbitIMG.jpg'
-        }
-        const petConfirm = document.querySelector('#petConfirm');
-        petConfirm.textContent = petConfirm.textContent + ` and is a ${petType}`; //make it so you can only select type once
     }
+    const petConfirm = document.querySelector('#petConfirm');
+    petConfirm.textContent = petConfirm.textContent + ` and is a ${pet.petType}`; //make it so you can only select type once
+}
+
+function calcHappiness(){
+    pet.happiness=(pet.dirtiness+pet.hunger+pet.sleep)/3;
+    const happyMeter = document.querySelector("#happiness");
+    const hungerMeter = document.querySelector("#hunger");
+    const energyMeter = document.querySelector("#energy");
+    const dirtMeter = document.querySelector("#dirtiness");
+    happyMeter.value = pet.happiness;
+    hungerMeter.value = pet.hunger;
+    energyMeter.value = pet.sleep;
+    dirtMeter.value = pet.dirtiness;
+}
 
 function init(){
     const nameButton = document.querySelector('.submit');
@@ -36,6 +51,7 @@ function init(){
     console.log(nameButton);
     nameButton.addEventListener('click', getName);
     petSelect.addEventListener('change', getType);
+    calcHappiness();
 }
 
 window.addEventListener('load', init);
