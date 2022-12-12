5fd01884bf5fdfdfdd4cf7182b237c6fd44d42c9
Whoosh12 <49247827+Whoosh12@users.noreply.github.com>
2022-12-12 10:53:03 +0000

    Cleaned up code

--- index.js
+++ index.js
@@ -7,29 +7,25 @@ const pet={
     happiness: 33
 }
 
-function getPet(){
+function getPet(){ //create an el to put all query selectors in on load loop over query selector all [id]
     const nameInput = document.querySelector('.name');
     const petConfirm = document.querySelector('#petConfirm');
     const petSelector = document.querySelector('.select');
     const nameButton = document.querySelector('.submit');
     const warning = document.createElement('h1');
+    warning.classList.add('warning');
+    document.querySelector('#petCreate').append(warning);
     if(nameInput.value=='' && petSelector.value=='')
     {
         warning.textContent = 'You have not inputted a name or selected a type.';
-        warning.classList.add('warning');
-        document.querySelector('#petCreate').append(warning);
     }
     else if(nameInput.value=='')
     {
         warning.textContent = 'You have not inputted a name.';
-        warning.classList.add('warning');
-        document.querySelector('#petCreate').append(warning);
     }
     else if(petSelector.value=='')
     {
         warning.textContent = 'You have not selected a type.'
-        warning.classList.add('warning');
-        document.querySelector('#petCreate').append(warning);
     }
     else{
         if(document.querySelector('.warning')){
@@ -39,7 +35,7 @@ function getPet(){
         pet.petType = petSelector.value;
         pet.petName = nameInput.value;
         document.querySelector('.select').style.display = 'none';
-        nameButton.style.display = 'none';
+        nameButton.style.display = 'none'; //put in css using class toggles
         nameInput.style.display = 'none';
         petConfirm.textContent = `Your ${pet.petType} is called ${pet.petName}.`; 
     }
@@ -47,7 +43,7 @@ function getPet(){
 
 function meterCalc(){
     pet.happiness=(pet.dirtiness+pet.hunger+pet.sleep)/3;
-    const happyMeter = document.querySelector("#happiness");
+    const happyMeter = document.querySelector("#happiness"); //el class
     const hungerMeter = document.querySelector("#hunger");
     const energyMeter = document.querySelector("#energy");
     const dirtMeter = document.querySelector("#dirtiness");
@@ -58,34 +54,18 @@ function meterCalc(){
 }
 
 function meterUpdater(){
-    if(pet.hunger > 0){
-        pet.hunger -= 1;
-    }
-    if(pet.dirtiness > 0){
-        pet.dirtiness -= 1;
-    }
-    if(pet.sleep > 0){
-        pet.sleep -= 1;
-    }
+    pet.hunger = Math.max(pet.hunger -= 1, 0);
+    pet. dirtiness = Math.max(pet.dirtiness -= 1, 0);
+    pet.sleep = Math.max(pet.sleep -= 1, 0);
     meterCalc();
 }
 
 function feedPet(){
-    if(pet.hunger > 75){
-        pet.hunger += 100 - pet.hunger;
-    }
-    else{
-        pet.hunger += 25;
-    }
+    pet.hunger = Math.min(pet.hunger += 25, 100);
 }
 
 function cleanPet(){
-    if(pet.dirtiness > 98){
-        pet.dirtiness += 100 - pet.dirtiness;
-    }
-    else{
-        pet.dirtiness += 2;
-    }
+    pet.dirtiness = Math.min(pet.dirtiness += 2, 100);
 }
 
 function init(){
